8e34cd16ce019af22dcc655fa6d136a0a0d6c56b
anthony-th <yefremov.anthony@gmail.com>
2023-06-17 06:05:03 +0300

    feat: 8functions-n-closures complete (Sat, Jun 17, 2023 06:05:02 AM)

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -161,8 +161,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function Log(...args) {
+    const newArg = args.map((arg) => JSON.stringify(arg)).join(',');
+    logFunc(`${func.name}(${newArg}) starts`);
+    const result = func.apply(this, args);
+    logFunc(`${func.name}(${newArg}) ends`);
+    return result;
+  };
 }
 
 
