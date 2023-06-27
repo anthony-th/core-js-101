527cec29bb56c733c7b73a2dce4d26a551c62f44
anthony-th <yefremov.anthony@gmail.com>
2023-06-27 03:10:02 +0300

    feat: 07 part complete (Tue, Jun 27, 2023 03:10:02 AM)

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -308,8 +308,12 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const ccnNumber = ccn.toString().split('').map(Number);
+  return ccnNumber.reduce((sum, num, index) => {
+    const processNum = index % 2 === ccnNumber.length % 2 ? num * 2 : num;
+    return sum + (processNum > 9 ? processNum - 9 : processNum);
+  }, 0) % 10 === 0;
 }
 
 /**
